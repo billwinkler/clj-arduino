8cc17310f2f5f946b0434a34ef5a999eee3fdd33
Bill Winkler <bill.winkler@gmail.com>
2021-01-22 22:45:19 -0600

    appears to work

--- src/clj_arduino/hx711.clj
+++ src/clj_arduino/hx711.clj
@@ -163,61 +163,6 @@
 ;;      (go (when-let [value (<!! out)] (println value)))
       )))
 
-;;(def msg-callback (message-handler))
-
-
-#_(def msg-callback
-  (let [debug false
-        qd 15  ;; queue depth for averaging tare value
-        ci 100 ;; calibration interval
-        n (atom 0)
-        tare-threshold 268205000 ;; assume scale is not cleared yet
-        state (atom :init)
-        hist (ref clojure.lang.PersistentQueue/EMPTY)
-        tare (atom 0)
-        calib (atom 1)
-        last-reading (atom 0)
-        weight (atom 0)
-        delta-threshold 75
-        take-reading (fn [value] (when (> (count @hist) 5)
-                              (cond (< delta-threshold (Math/abs (- @last-reading value)))
-                                    (do (reset! last-reading value)
-                                        (repeatedly (count @hist) #(dosync (alter hist pop)))
-                                        (println (fmt-float @weight) value)))))]
-    (fn [msg]
-      (swap! n inc)
-      (let [value (decode-msg msg)]
-        (when (= qd (count @hist)) (dosync (alter hist pop)))
-        (dosync (alter hist conj value))
-        (reset! weight (/ (- (avg @hist) @tare) @calib))
-
-        (condp = @state
-          :init       (cond (> value tare-threshold)
-                            (do (println "initializing" value)
-                                (reset! state :tare-wait))
-                            :else (reset! state :tare-begin))
-          :tare-wait  (cond (< value tare-threshold) (reset! state :tare-begin)
-                            (zero? (mod @n 10)) (println "clear the scale!" value))
-          :tare-begin (do (reset! n 0) (reset! state :tare-running))
-          :tare-running (when (> @n qd)
-                          (reset! tare (avg @hist))
-                          (println "tare value is:" (fmt-float @tare))
-                          (reset! state :calib-begin))
-          :calib-begin (do (reset! n 0) (reset! state :calib-wait)
-                           (println "place 25g reference weight on scale..."))
-          :calib-wait (cond (> (- value @tare) 1000)
-                            (do (reset! state :calib-pend)
-                                (reset! n 0)
-                                (println "weight detected..." value (fmt-float @tare) (- value @tare)))
-                            (zero? (mod @n 50)) (println "waiting for calibration weight!" value))
-          
-          :calib-pend (when (> @n 10) (do (reset! state :calibrating) (reset! n 0)))
-          :calibrating (when (> @n 10) (do (reset! state :running) (reset! n 0)
-                                           (reset! calib (calibration-factor 25.0 @tare value))
-                                           (println "calibration factor set" (fmt-float @calib))))
-          :running (take-reading value))
-
-        (when (and debug (zero? (mod @n 10))) (println @n @state @weight))))))
 
 (defn state-change-alert
   "a var watcher to detect shutdown event"
