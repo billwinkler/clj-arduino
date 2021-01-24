36fb6f8c89bda19bc69a5f42426c95ef7c821f75
Bill Winkler <bill.winkler@gmail.com>
2021-01-24 13:19:50 -0600

    snapshot

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-arduino "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-arduino "0.1.2-SNAPSHOT"
+  :description "A Clojure Arduino prototyping library"
+  :url "https://com.github.billwinkler/clj-arduino"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-;;                 [clodiuno "0.0.4-SNAPSHOT"]
+                 ;; [clodiuno "0.0.4-SNAPSHOT"]
                  [com.github.billwinkler/clodiuno "0.0.4-with-port-id-fix"]
-                  [org.clojure/core.async "1.3.610"]]
+                 [org.clojure/core.async "1.3.610"]]
   :repl-options {:init-ns clj-arduino.core}
   :repositories [["jitpack" "https://jitpack.io"]])
