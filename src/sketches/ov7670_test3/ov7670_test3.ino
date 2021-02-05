0972b74969de82c56f07ea45d8da800fe814de4c
Bill Winkler <bill.winkler@gmail.com>
2021-02-05 06:57:30 -0600

    with timing printouts
    before messing this up

--- src/sketches/ov7670_test3/ov7670_test3.ino
+++ src/sketches/ov7670_test3/ov7670_test3.ino
@@ -1,7 +1,14 @@
 
+unsigned long lastTriggerTime;
+volatile unsigned long triggerTime;
+volatile boolean triggered;
+uint8_t measurements = 0;
+
+volatile boolean write_to_1;
+
 uint16_t vcnt = 0;
 uint16_t pcnt = 0;
-uint16_t bcnt = 0;
+volatile uint16_t bcnt = 0;
 
 unsigned long t0;
 unsigned long dt;
@@ -14,7 +21,50 @@ uint8_t buf2[128];
 uint8_t*bp;       // a pointer to an unsigned byte type
 uint8_t*curr_bp;  // last buffer points to the indicator
 
-void vsync() {
+void timer () 
+{
+  // wait until we noticed last one
+  if (triggered)
+    return;
+
+  triggerTime = micros ();
+  triggered = true;
+}  // end of timer
+
+void vsync () 
+{
+  if (triggered)
+    return;
+
+  triggerTime = micros ();
+  triggered = true;
+  bcnt = 0;
+  write_to_1 = true;
+}  // end of vsync
+
+void pclk () 
+{
+  if (!triggered)
+    return;
+
+  // write the data to the buffer
+  *bp = (PINC&15)|(PIND&240);
+  
+  if ((bp - curr_bp) < sizeof(buf1)) {
+    ++bp; 
+  } else {
+    // increment buffer counter
+    ++bcnt;
+    write_to_1 = !write_to_1;    
+    if (write_to_1) {
+      bp = &buf1[0];
+    } else {
+      bp = &buf2[0];
+    } 
+  }
+}  // end of pclk
+
+void vsync1() {
   sync ^= 1;
   ++vcnt;
   bcnt = 0;
@@ -26,9 +76,9 @@ void vsync() {
    curr_bp = bp;     
 }
 
-void pclk() {
+void pclk1() {
   // write the data to the buffer
-  *bp =(PINC&15)|(PIND&240);
+  *bp = (PINC&15)|(PIND&240);
 
   if ((bp - curr_bp) < sizeof(buf1)) {
     ++bp; 
@@ -86,10 +136,101 @@ void setup() {
   pinMode(vsyncPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(vsyncPin), vsync, FALLING);
   attachInterrupt(digitalPinToInterrupt(pcklPin), pclk, FALLING);
+//  attachInterrupt(digitalPinToInterrupt(vsyncPin), timer, FALLING);
+//  attachInterrupt(digitalPinToInterrupt(pcklPin), timer, FALLING);
 
 }
 
+void pr_pindc() {
+  byte pins_d = PIND;
+  byte pins_c = PINC;
+  Serial.print( (pins_d >> 7) & 1 );
+  Serial.print( (pins_d >> 6) & 1 );
+  Serial.print( (pins_d >> 5) & 1 );
+  Serial.print( (pins_d >> 4) & 1 ); 
+  Serial.print( (pins_d >> 3) & 1 );
+  Serial.print( (pins_d >> 2) & 1 );      
+  Serial.print( (pins_d >> 1) & 1 );      
+  Serial.print( pins_d & 1 );
+  Serial.print( " " );            
+  Serial.print( (pins_c >> 7) & 1 );
+  Serial.print( (pins_c >> 6) & 1 );
+  Serial.print( (pins_c >> 5) & 1 );
+  Serial.print( (pins_c >> 4) & 1 ); 
+  Serial.print( (pins_c >> 3) & 1 );
+  Serial.print( (pins_c >> 2) & 1 );      
+  Serial.print( (pins_c >> 1) & 1 );      
+  Serial.println( pins_c & 1 );  
+  } //end pr_pindc
+
+void pr_data(byte data) {
+  Serial.print( (data >> 7) & 1 );
+  Serial.print( (data >> 6) & 1 );
+  Serial.print( (data >> 5) & 1 );
+  Serial.print( (data >> 4) & 1 ); 
+  Serial.print( (data >> 3) & 1 );
+  Serial.print( (data >> 2) & 1 );      
+  Serial.print( (data >> 1) & 1 );      
+  Serial.println(data & 1 );    
+  } //end pr_data
+
 void loop() {
+    if (measurements > 10) 
+    return;
+    
+    if (!triggered)
+    return;
+
+    //detachInterrupt(digitalPinToInterrupt(3));
+    //Serial.println("triggered");  
+    //while(!(PIND&8));//wait for vs high
+    //while(!(PIND & B10000000));//wait for D7 high 
+//    while(!(PIND & B00001000));//wait for vs high 
+//    while(!(PIND & B00000100));//wait for pckl high 
+    //while(!(PIND & B00010000));//wait for D4 high 
+    
+  unsigned long elapsed = micros() - triggerTime;
+  ++measurements;
+
+  
+  if (elapsed < 20L)
+    {
+    triggered = false;
+    return;  // ignore if less than 20 microseconds
+    }
+    
+  triggered = false;  // re-arm for next time
+  //attachInterrupt(digitalPinToInterrupt(3), timer, FALLING);
+  
+  Serial.print ("Took: ");
+  Serial.print (elapsed);
+  Serial.print (" microseconds. ");
+  
+  unsigned long minutes, seconds, ms;
+  
+  minutes = elapsed / (1000000L * 60);
+  elapsed -= minutes * (1000000L * 60);
+  
+  seconds = elapsed / 1000000L;
+  elapsed -= seconds * 1000000L;
+  
+  ms = elapsed / 1000;
+  elapsed -= ms * 1000;
+  
+  Serial.print (minutes);
+  Serial.print ("m ");
+  Serial.print (seconds);
+  Serial.print ("s ");
+  Serial.print (ms);
+  Serial.println ("ms.");
+
+  for (int x = 0; x < 10; x++) {
+      Serial.print(micros() - triggerTime);
+      Serial.print(" ");
+      pr_data((PINC&15)|(PIND&240));      
+  }
+
+  /*
   int lcnt = 0;
 
   uint8_t*bp2,*bp3;
@@ -128,6 +269,7 @@ void loop() {
  
     Serial.println();
   }
+  */
     
 }
 
